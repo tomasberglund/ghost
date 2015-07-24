d739d919a41b4ff23b876f0c8275f9d569581818
Tomas Berglund <tomas@abouttime.se>
2015-07-24 22:15:19 +0200

    inital commit

---
+++ examples/controllers/homeController.php
@@ -0,0 +1,18 @@
+<?php namespace Controllers;
+
+class HomeController
+{
+
+    function getIndex()
+    {
+        echo 'index';
+    }
+
+    function getPage()
+    {
+        echo 'page';
+    }
+
+}
+
+# end
---
+++ examples/index.php
@@ -0,0 +1,8 @@
+<?php
+# init composer autoloader
+require __DIR__ . "/vendor/autoload.php";
+
+# serve app
+dispatch();
+
+# end
